31510a00fc4f4fd69e0b74b4917fae862a55a1c1
arnoldasjan <arnuske@yahoo.com>
2021-03-20 23:22:51 +0200

    added additional data

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def hello_world():
     return '{"starting point":58}'
 
 
+@app.route('/get/traverse')
+def traverse_data():
+    return '''
+    {
+        "video_start": 58,
+        "highest_point": 2.56
+    }
+    '''
+
+
 if __name__ == '__main__':
     app.run()
